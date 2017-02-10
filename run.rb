60ae05202823808bb8097403a87a38cdbace1a01
Alex <1sasha.sasha1@gmail.com>
2017-02-10 12:12:02 +0200

    Added cube creation mechanism

--- run.rb
+++ run.rb
@@ -8,10 +8,13 @@ include Gl, Glu
 
 class Window < Gosu::Window
 
+  attr_accessor :scene_objects
+
   def initialize
     super 800, 600
     self.caption = "Diatom's OpenGL Tutorial"
     @camera = Camera.new(self)
+    @scene_objects = []
   end
 
   def update
@@ -30,25 +33,35 @@ class Window < Gosu::Window
     	glLoadIdentity()
       @camera.capture
 
+      @scene_objects << Axis.new(100, 100, 100)
+      @scene_objects << Cube.new(50, 50, 50, GL_FRONT_AND_BACK, GL_FILL)
+
       glPushMatrix
         glTranslated(width/2, height/2, 0)
-        Axis.draw(100, 100, 100)
-        Cube.draw(50, 50, 50, GL_FRONT_AND_BACK, GL_FILL)
-        # puts "#{@camera.look_at_object_position.x} #{@camera.look_at_object_position.y} #{@camera.look_at_object_position.z}"
+        @scene_objects.each do |object|
+          object.draw
+        end
       glPopMatrix
       Cube.draw(50, 50, 50, GL_FRONT_AND_BACK, GL_LINE, @camera.look_at_object_position.x, @camera.look_at_object_position.y, @camera.look_at_object_position.z)
 
+      # @scene_objects << Cube.new(50, 50, 50, GL_FRONT_AND_BACK, GL_FILL)
 
-
-      Cube.draw(50, 50, 50, GL_FRONT_AND_BACK, GL_FILL)
-
-      Axis.draw(100, 100, 100, true)
+      # Axis.draw(100, 100, 100, true)
 
     end
   end
 
   def button_down(id)
     exit if id == Gosu::KbEscape
+    add_cube if id == Gosu::KbSpace
+  end
+
+  def add_cube
+    @scene_objects << Cube.new(
+      50, 50, 50,
+      GL_FRONT_AND_BACK, GL_FILL,
+      @camera.look_at_object_position.x - width/2, @camera.look_at_object_position.y - height/2, @camera.look_at_object_position.z
+    )
   end
 
 end
